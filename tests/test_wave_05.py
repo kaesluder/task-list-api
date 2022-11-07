9f75eb316ba887dfcb40813c2f8c6ebbd8f00ff5
Kae Sluder <kaesluder@fastmail.com>
2022-11-07 11:45:24 -0500

    testing missing title on goal creation

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -141,7 +141,7 @@ def test_delete_goal_not_found(client):
     # ---- Complete Assertions Here ----
 
 
-@pytest.mark.skip(reason="No way to test this feature yet")
+# @pytest.mark.skip(reason="No way to test this feature yet")
 def test_create_goal_missing_title(client):
     # Act
     response = client.post("/goals", json={})
